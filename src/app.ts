b37e3559795a4fcd20dcb5bdc476829b9bde8350
krystianslovik <krystian.slowik@trustedshops.com>
2024-03-12 00:19:33 +0100

    project item introduction
    refactor

--- src/app.ts
+++ src/app.ts
@@ -146,6 +146,33 @@ abstract class Component<T extends HTMLElement, U extends HTMLElement> {
   abstract renderContent(): void;
 }
 
+//Project Item
+
+class ProjectItem extends Component<HTMLUListElement, HTMLLIElement> {
+  private project: Project;
+
+  get persons() {
+    if (this.project.people === 1) {
+      return "1 person";
+    } else {
+      return `${this.project.people} people assign`;
+    }
+  }
+
+  constructor(hostId: string, project: Project) {
+    super("single-project", hostId, false, project.id);
+    this.project = project;
+    this.renderContent();
+  }
+
+  configure(): void {}
+  renderContent(): void {
+    this.element.querySelector("h2")!.textContent = this.project.title;
+    this.element.querySelector("h3")!.textContent = `${this.persons} `;
+    this.element.querySelector("p")!.textContent = this.project.description;
+  }
+}
+
 // Project List class
 class ProjectList extends Component<HTMLDivElement, HTMLElement> {
   assignedProjects: Project[] = [];
@@ -184,9 +211,7 @@ class ProjectList extends Component<HTMLDivElement, HTMLElement> {
     );
     listEl.innerHTML = "";
     for (const prjItm of this.assignedProjects) {
-      const listItem = document.createElement("li");
-      listItem.textContent = prjItm.title;
-      listEl.appendChild(listItem);
+      new ProjectItem(this.element.querySelector("ul")!.id, prjItm);
     }
   }
 }
